3922d68f41203a4c1848581088c9229f0ed83991
Daehoon Song <dhsong@Song-MacBookPro.local>
2020-03-04 12:23:17 +0900

    List Test Done

--- root/tests/views.py
+++ root/tests/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Test
+from .models import Question
+
 
 # Create your views here.
+class TestListView(ListView):
+    model = Test
